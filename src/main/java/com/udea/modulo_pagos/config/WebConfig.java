0f022166189c565cb7949e88d0fbfe0ece552063
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2024-11-13 22:49:32 -0500

    Update WebConfig.java

--- src/main/java/com/udea/modulo_pagos/config/WebConfig.java
+++ src/main/java/com/udea/modulo_pagos/config/WebConfig.java
@@ -6,14 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CORSConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://sandbox.wompi.co/v1/transactions") // O el origen que estés usando
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*"); 
     }
 }
