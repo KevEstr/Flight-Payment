c33b1b8724999414311dd0fe5ff890532c794441
apoyodesarrollo6.ing <apoyodesarrollo6.ing@udea.edu.co>
2024-11-25 18:14:22 -0500

    integration tests added again again

--- src/test/java/com/udea/modulo_pagos/ModuloPagosIntegrationTests.java
+++ src/test/java/com/udea/modulo_pagos/ModuloPagosIntegrationTests.java
@@ -4,6 +4,8 @@ import com.udea.modulo_pagos.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -23,6 +25,13 @@ class ModuloPagosIntegrationTests {
             .withDatabaseName("testdb")
             .withUsername("testuser")
             .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Autowired
     private IPaymentRepository paymentRepository;
